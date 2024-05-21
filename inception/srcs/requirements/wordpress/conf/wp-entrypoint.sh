68f6d3ec28ec7424c02f4cf37ac2f2b035c5a2f8
rchbouki <rchbouki@rchbouki.42.fr>
2024-05-21 22:15:27 +0200

    lets go i think we may be done

--- inception/srcs/requirements/wordpress/conf/wp-entrypoint.sh
+++ inception/srcs/requirements/wordpress/conf/wp-entrypoint.sh
@@ -4,8 +4,11 @@
 if ! $(wp core is-installed); then
 	wp core install --url="$DOMAIN_NAME" --title="Riri's Inception" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_MAIL"
 	wp user create $NEW_USER $USER_MAIL --user_pass=$USER_PASS --allow-root
-
 	wp theme install writers-haven --activate --allow-root
+	# Install and activate Redis Cache plugin
+	wp plugin install wp-redis --activate --allow-root
+	# Enable Redis caching
+	wp wp-redis enable --allow-root
 fi
 
 # Start PHP-FPM
